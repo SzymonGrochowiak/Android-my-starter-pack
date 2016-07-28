daefd91d580b0441316e4bc496cfea3b657ddb0d
Szymon <szymong89@gmail.com>
2016-07-29 00:03:09 +0200

    Add retrofit dependency

--- app/src/main/java/com/szymongrochowiak/androidstarterpack/activities/MainActivity.java
+++ app/src/main/java/com/szymongrochowiak/androidstarterpack/activities/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         ((StarterPackApplication) getApplication()).getDaggerApplicationComponent().inject(this);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
