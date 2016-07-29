93683f6196a7c4cd7a01c36dbfaee8bc37b63b65
Szymon Grochowiak <szymon.grochowiak@gmail.com>
2016-07-29 23:17:07 +0200

    Add Retrofit and RxJava dependencies, add fundamental classes for retrofit and networking

---
+++ app/src/main/java/com/szymongrochowiak/androidstarterpack/NetworkingModule.java
@@ -0,0 +1,14 @@
+package com.szymongrochowiak.androidstarterpack;
+
+import dagger.Module;
+
+/**
+ * @author Szymon Grochowiak
+ */
+@Module
+public class NetworkingModule {
+
+    public static final String BASE_ENDPOINT = "...";
+
+
+}
