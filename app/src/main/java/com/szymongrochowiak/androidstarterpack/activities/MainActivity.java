0084c804f9c587e4a3de3beec29dc7cdcb7abaa6
Szymon Grochowiak <szymon.grochowiak@gmail.com>
2016-08-03 21:58:46 +0200

    Replace left interface implementation by lambda statement

--- app/src/main/java/com/szymongrochowiak/androidstarterpack/activities/MainActivity.java
+++ app/src/main/java/com/szymongrochowiak/androidstarterpack/activities/MainActivity.java
@@ -49,13 +49,8 @@ public class MainActivity extends BaseActivity
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
