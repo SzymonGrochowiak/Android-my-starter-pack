02c5f52995e712b2f4bd35cd683d0871a7ffad17
Szymon Grochowiak <szymon.grochowiak@gmail.com>
2017-04-07 20:39:19 +0200

    handling screen orientation changes

--- app/src/main/java/com/szymongrochowiak/androidstarterpack/ui/main/MainPresenter.java
+++ app/src/main/java/com/szymongrochowiak/androidstarterpack/ui/main/MainPresenter.java
@@ -1,13 +1,16 @@
 package com.szymongrochowiak.androidstarterpack.ui.main;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.szymongrochowiak.androidstarterpack.data.ApplicationRepository;
+import com.szymongrochowiak.androidstarterpack.data.model.Berry;
 import com.szymongrochowiak.androidstarterpack.ui.common.mvp.BasePresenter;
 
 import java.util.Random;
 
 import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import timber.log.Timber;
 
 /**
@@ -18,23 +21,48 @@ public class MainPresenter extends BasePresenter<MainView> {
     @NonNull
     private ApplicationRepository mRepository;
 
+    @Nullable
+    private Berry mBerry;
+    @Nullable
+    private String mErrorMessage;
+
     public MainPresenter(@NonNull ApplicationRepository repository) {
         mRepository = repository;
     }
 
     public void queryBerry() {
-        Subscription fetchBerrySubscription = mRepository.queryBerry(getBerryId())
+        if (restoreViewStateIfExist()) {
+            return;
+        }
+        Subscription fetchBerrySubscription = mRepository.queryBerry(getBerryId()).observeOn(AndroidSchedulers
+                .mainThread())
                 .subscribe(berry -> {
+                    mBerry = berry;
                     if (isViewAttached()) {
                         getView().showBerryName(berry.getName());
                     }
                 }, throwable -> {
                     Timber.e(throwable);
+                    mErrorMessage = throwable.toString();
                     if (isViewAttached()) {
-                        getView().showBerryFetchError(throwable.toString());
+                        getView().showBerryFetchError(mErrorMessage);
                     }
                 });
         getCompositeSubscription().add(fetchBerrySubscription);
+    }
+
+    private boolean restoreViewStateIfExist() {
+        if (isViewAttached()) {
+            if (mBerry != null) {
+                getView().showBerryName(mBerry.getName());
+                return true;
+            }
+            if (mErrorMessage != null) {
+                getView().showBerryFetchError(mErrorMessage);
+                return true;
+            }
+        }
+        return false;
     }
 
     private int getBerryId() {
