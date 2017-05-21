9581f93a05c811d4a33ea3fd597ca6b22f32e549
Szymon Grochowiak <szymon.grochowiak@gmail.com>
2017-05-21 16:09:22 +0200

    Disable animations rule for android test - fix granting access

--- app/src/androidTest/java/com/szymongrochowiak/androidstarterpack/test/utils/DisableAnimationsRule.java
+++ app/src/androidTest/java/com/szymongrochowiak/androidstarterpack/test/utils/DisableAnimationsRule.java
@@ -1,6 +1,8 @@
 package com.szymongrochowiak.androidstarterpack.test.utils;
 
 import android.app.Instrumentation;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.uiautomator.UiDevice;
@@ -18,6 +20,9 @@ import java.util.Arrays;
  */
 public class DisableAnimationsRule implements TestRule {
 
+    private final static String SET_ANIMATION_SCALE_PERMISSION = "android.permission.SET_ANIMATION_SCALE";
+    private final static String GRANT_PERMISSION_COMMAND = "pm grant %s %s";
+
     private final static float ANIMATION_ENABLED = 1.0f;
     private final static float ANIMATION_DISABLED = 0.0f;
 
@@ -26,19 +31,7 @@ public class DisableAnimationsRule implements TestRule {
     private Object mWindowManagerObject;
 
     public DisableAnimationsRule() {
-        try {
-            Class<?> windowManagerStubClazz = Class.forName("android.view.IWindowManager$Stub");
-            Method asInterface = windowManagerStubClazz.getDeclaredMethod("asInterface", IBinder.class);
-            Class<?> serviceManagerClazz = Class.forName("android.os.ServiceManager");
-            Method getService = serviceManagerClazz.getDeclaredMethod("getService", String.class);
-            Class<?> windowManagerClazz = Class.forName("android.view.IWindowManager");
-            mSetAnimationScalesMethod = windowManagerClazz.getDeclaredMethod("setAnimationScales", float[].class);
-            mGetAnimationScalesMethod = windowManagerClazz.getDeclaredMethod("getAnimationScales");
-            IBinder windowManagerBinder = (IBinder) getService.invoke(null, "window");
-            mWindowManagerObject = asInterface.invoke(null, windowManagerBinder);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to access animation methods", e);
-        }
+        reflectAnimationMethods();
     }
 
     @Override
@@ -58,15 +51,38 @@ public class DisableAnimationsRule implements TestRule {
     }
 
     private void setAnimationScaleFactors(float scaleFactor) throws Exception {
-        float[] scaleFactors = (float[]) mGetAnimationScalesMethod.invoke(mWindowManagerObject);
-        Arrays.fill(scaleFactors, scaleFactor);
-        mSetAnimationScalesMethod.invoke(mWindowManagerObject, new Object[]{scaleFactors});
+        Context context = InstrumentationRegistry.getInstrumentation().getContext();
+
+        int permStatus = context.checkCallingOrSelfPermission(SET_ANIMATION_SCALE_PERMISSION);
+        if (permStatus == PackageManager.PERMISSION_GRANTED) {
+            float[] scaleFactors = (float[]) mGetAnimationScalesMethod.invoke(mWindowManagerObject);
+            Arrays.fill(scaleFactors, scaleFactor);
+            mSetAnimationScalesMethod.invoke(mWindowManagerObject, new Object[]{scaleFactors});
+        } else {
+            throw new RuntimeException("Cannot grant permission " + SET_ANIMATION_SCALE_PERMISSION);
+        }
     }
 
     private void grantSetAnimationScalePermission() throws IOException {
         Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
         String packageName = instrumentation.getTargetContext().getPackageName();
-        UiDevice.getInstance(instrumentation)
-                .executeShellCommand("pm grant " + packageName + " android.permission.SET_ANIMATION_SCALE");
+        String command = String.format(GRANT_PERMISSION_COMMAND, packageName, SET_ANIMATION_SCALE_PERMISSION);
+        UiDevice.getInstance(instrumentation).executeShellCommand(command);
+    }
+
+    private void reflectAnimationMethods() {
+        try {
+            Class<?> windowManagerStubClazz = Class.forName("android.view.IWindowManager$Stub");
+            Method asInterface = windowManagerStubClazz.getDeclaredMethod("asInterface", IBinder.class);
+            Class<?> serviceManagerClazz = Class.forName("android.os.ServiceManager");
+            Method getService = serviceManagerClazz.getDeclaredMethod("getService", String.class);
+            Class<?> windowManagerClazz = Class.forName("android.view.IWindowManager");
+            mSetAnimationScalesMethod = windowManagerClazz.getDeclaredMethod("setAnimationScales", float[].class);
+            mGetAnimationScalesMethod = windowManagerClazz.getDeclaredMethod("getAnimationScales");
+            IBinder windowManagerBinder = (IBinder) getService.invoke(null, "window");
+            mWindowManagerObject = asInterface.invoke(null, windowManagerBinder);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to access animation methods", e);
+        }
     }
 }
