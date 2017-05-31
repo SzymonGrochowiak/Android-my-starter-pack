37d7bab2de678521eb93b863d59d759be7e04126
Szymon Grochowiak <szymon.grochowiak@gmail.com>
2017-05-31 21:10:50 +0200

    rx idling resource

--- app/src/androidTest/java/com/szymongrochowiak/androidstarterpack/ui/main/MainActivityTest.java
+++ app/src/androidTest/java/com/szymongrochowiak/androidstarterpack/ui/main/MainActivityTest.java
@@ -1,14 +1,25 @@
 package com.szymongrochowiak.androidstarterpack.ui.main;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 
+import com.szymongrochowiak.androidstarterpack.R;
 import com.szymongrochowiak.androidstarterpack.test.utils.DisableAnimationsRule;
-import com.szymongrochowiak.androidstarterpack.test.utils.RxJavaImmediateSchedulersRule;
+import com.szymongrochowiak.androidstarterpack.test.utils.RxIdlingResource;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+
+import io.reactivex.plugins.RxJavaPlugins;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.not;
 
 /**
  * @author Szymon Grochowiak
@@ -17,13 +28,23 @@ public class MainActivityTest {
 
     @ClassRule
     public static DisableAnimationsRule disableAnimationsRule = new DisableAnimationsRule();
+
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class, false, false);
+
+
+    @Before
+    public void before() {
+        RxIdlingResource rxIdlingResource = new RxIdlingResource();
+        Espresso.registerIdlingResources(rxIdlingResource);
+        RxJavaPlugins.setScheduleHandler(rxIdlingResource);
+    }
 
     @Test
     public void show_content() throws Exception {
         mActivityTestRule.launchActivity(null);
-        Thread.sleep(5000);
-        Assert.assertEquals(1, 0);
+
+        onView(withId(R.id.progressBar)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.textView)).check(matches(withText("mock leppa")));
     }
 }
